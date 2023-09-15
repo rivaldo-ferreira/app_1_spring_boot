21755f52ee3ed98341d0cd89f57eecffd0793620
Rivas <rivaldo13@gmail.com>
2023-09-15 12:17:13 -0300

    Arrumando o OrderStatus

--- src/main/java/com/projeto_01/demo/entities/Order.java
+++ src/main/java/com/projeto_01/demo/entities/Order.java
@@ -28,7 +28,7 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
 	private Instant moment;
 	
-	private OrderStatus orderStatus;
+	private Integer orderStatus;
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -40,7 +40,7 @@ public class Order implements Serializable{
 	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
-		this.orderStatus = orderStatus;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -62,11 +62,13 @@ public class Order implements Serializable{
 	
 
 	public OrderStatus getOrderStatus() {
-		return orderStatus;
+		return OrderStatus.valueOf(orderStatus);
 	}
 
 	public void setOrderStatus(OrderStatus orderStatus) {
-		this.orderStatus = orderStatus;
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
